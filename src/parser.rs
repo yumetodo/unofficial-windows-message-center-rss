29a5c79ee5a55771c342e3edf985bd08f8680288
yumetodo <yume-wikijp@live.jp>
2023-08-15 21:17:50 +0900

    feat(test): add parser test

--- src/parser.rs
+++ src/parser.rs
@@ -102,22 +102,292 @@ impl Parser {
 #[cfg(test)]
 mod test {
     use super::*;
-    #[test]
-    fn parse_date() {
-        let input = r#"
+    use regex::Regex;
+    const HTML_OF_ABSOLUTE_PATH: &str = r##"
 <!DOCTYPE html>
 <html>
 <body>
 <h2 id="recent-announcements">Recent announcements</h2>
 <table border ='0'>
-<tr><td id='2832'>aaa</td><td class='has-no-wrap'>2022-05-24 <br>14:00 PT</td></tr>
+<tr><td id="3112">
+<a
+    href="/en-us/lifecycle/announcements/windows-10-21h2-end-of-servicing"
+    target="_blank"
+    data-linktype="absolute-path"
+    ><b
+        >Reminder: End of servicing for Windows 10, version
+        21H2 Home, Pro, Pro Education, and Pro for
+        Workstations</b
+    ></a
+></td><td class='has-no-wrap'>2022-05-24 <br>14:00 PT</td></tr>
 </table>
 </body>
-</html>"#;
-        let doc = Html::parse_document(input);
+</html>"##;
+    const HTML_OF_EXTERNAL: &str = r##"
+<!DOCTYPE html>
+<html>
+<body>
+<h2 id="recent-announcements">Recent announcements</h2>
+<table border ='0'>
+<tr><td id="3110">
+<a
+    href="https://support.microsoft.com/help/5027231"
+    target="_blank"
+    data-linktype="external"
+    ><b
+        >Take action: June 2023 security update is now available</b
+    ></a
+><a
+    class="docon docon-link heading-anchor"
+    aria-labelledby="3110"
+    href="#3110"
+    data-linktype="self-bookmark"
+></a
+><br />
+<div>
+    The June 2023 security update release is now available
+    for Windows 11 and all supported versions of Windows
+    10. We recommend that you install these updates
+    promptly. For more information about the contents of
+    this update, see the release notes, which are easily
+    accessible from the&nbsp;<a
+        href="https://support.microsoft.com/help/5027231"
+        rel="noopener noreferrer"
+        target="_blank"
+        data-linktype="external"
+        >Windows 11</a
+    >&nbsp;and&nbsp;<a
+        href="https://support.microsoft.com/help/5027215"
+        rel="noopener noreferrer"
+        target="_blank"
+        data-linktype="external"
+        >Windows 10</a
+    >&nbsp;update history pages. For instructions on how
+    to install this update on your home device, check
+    the&nbsp;<a
+        href="https://support.microsoft.com/windows/update-windows-3c5ae7fc-9fb6-9af1-1984-b5e0412c556a#WindowsVersion=Windows_11"
+        rel="noopener noreferrer"
+        target="_blank"
+        data-linktype="external"
+        >Update Windows</a
+    >&nbsp;article. To learn more about the different
+    types of monthly quality updates, see
+    <a
+        href="https://aka.ms/Windows/MonthlyUpdates"
+        rel="noopener noreferrer"
+        target="_blank"
+        data-linktype="external"
+        >Windows monthly updates explained</a
+    >. To be informed about the latest updates and
+    releases, follow us on Twitter <a
+        href="https://twitter.com/windowsupdate"
+        rel="noopener noreferrer"
+        target="_blank"
+        data-linktype="external"
+        ><u>@WindowsUpdate</u></a
+    >.
+</div>
+<div>&nbsp;&nbsp;</div>
+<div>
+    <strong
+        >Highlights for the Windows 11 update:&nbsp;</strong
+    >
+</div>
+<ul>
+    <li>
+        This update addresses security issues for your
+        Windows operating system.&nbsp;&nbsp;&nbsp;
+    </li>
+    <li>
+        This update addresses a known issue that affects
+        32-bit apps that are&nbsp;<a
+            href="/en-us/windows/win32/memory/memory-limits-for-windows-releases#memory-and-address-space-limits"
+            rel="noopener noreferrer"
+            target="_blank"
+            data-linktype="absolute-path"
+            >large address aware</a
+        >&nbsp;and use the&nbsp;<a
+            href="/en-us/windows/win32/api/winbase/nf-winbase-copyfile"
+            rel="noopener noreferrer"
+            target="_blank"
+            data-linktype="absolute-path"
+            >CopyFile API</a
+        >. You might have issues when you save, copy, or
+        attach files. If you use some commercial or
+        enterprise security software that uses extended file
+        attributes, this issue will likely affect you. For
+        Microsoft Office apps, this issue only affects the
+        32-bit versions. You might receive the error,
+        "Document not saved."&nbsp;
+    </li>
+</ul>
+<div></div>
+<div>
+    Short on time? Watch our short
+    <a
+        href="https://youtu.be/2CPFOqAoG4o"
+        rel="noopener noreferrer"
+        target="_blank"
+        data-linktype="external"
+        >Windows 11 update</a
+    >
+    release notes video for this month's tips.
+</div>
+</td><td class='has-no-wrap'>2022-05-24 <br>14:00 PT</td></tr>
+</table>
+</body>
+</html>"##;
+    const HTML_OF_SELF_BOOKMARK: &str = r##"
+<!DOCTYPE html>
+<html>
+<body>
+<h2 id="recent-announcements">Recent announcements</h2>
+<table border ='0'>
+<tr><td id="3113">
+<b
+    >Reminder: Security hardening changes for Netlogon and Kerberos coming in June and July 2023</b
+><a
+    class="docon docon-link heading-anchor"
+    aria-labelledby="3113"
+    href="#3113"
+    data-linktype="self-bookmark"
+></a
+></td><td class='has-no-wrap'>2022-05-24 <br>14:00 PT</td></tr>
+</table>
+</body>
+</html>"##;
+    #[test]
+    fn parse_date() {
+        let doc = Html::parse_document(HTML_OF_EXTERNAL);
         let selector = Selector::parse("td[id] + td").unwrap();
         let elem = doc.select(&selector).next().unwrap();
         let parsed = Parser::new("", "").parse_date(elem);
         assert_eq!(parsed, Some("2022-05-24T21:00:00Z".to_string()));
+    }
+    #[test]
+    fn parse_url_when_absolute_path() {
+        let doc = Html::parse_document(HTML_OF_ABSOLUTE_PATH);
+        let p = Parser::new("", "https://docs.microsoft.com");
+        let title_element = doc
+            .select(&p.selector_table_line)
+            .next()
+            .unwrap()
+            .select(&p.selector_article_head)
+            .next()
+            .unwrap();
+        let ret = p.parse_url(title_element);
+        assert!(ret.is_some());
+        let (url, rel) = ret.unwrap();
+        assert_eq!(url, "https://docs.microsoft.com/en-us/lifecycle/announcements/windows-10-21h2-end-of-servicing");
+        assert_eq!(rel, "alternate");
+    }
+
+    #[test]
+    fn parse_url_when_external() {
+        let doc = Html::parse_document(HTML_OF_EXTERNAL);
+        let p = Parser::new("", "");
+        let title_element = doc
+            .select(&p.selector_table_line)
+            .next()
+            .unwrap()
+            .select(&p.selector_article_head)
+            .next()
+            .unwrap();
+        let ret = p.parse_url(title_element);
+        assert!(ret.is_some());
+        let (url, rel) = ret.unwrap();
+        assert_eq!(url, "https://support.microsoft.com/help/5027231");
+        assert_eq!(rel, "alternate");
+    }
+    #[test]
+    fn parse_url_when_self_bookmark() {
+        let doc = Html::parse_document(HTML_OF_SELF_BOOKMARK);
+        let p = Parser::new(
+            "https://yumetodo.github.io/unofficial-windows-message-center-rss/feed/atom10.xml",
+            "",
+        );
+        let title_element = doc
+            .select(&p.selector_table_line)
+            .next()
+            .unwrap()
+            .select(&p.selector_article_head)
+            .next()
+            .unwrap();
+        let ret = p.parse_url(title_element);
+        assert!(ret.is_some());
+        let (url, rel) = ret.unwrap();
+        assert_eq!(
+            url,
+            "https://yumetodo.github.io/unofficial-windows-message-center-rss/feed/atom10.xml"
+        );
+        assert_eq!(rel, "self");
+    }
+    #[test]
+    fn parse_title_when_alternate() {
+        let doc = Html::parse_document(HTML_OF_EXTERNAL);
+        let p = Parser::new("", "");
+        let tr: ElementRef<'_> = doc.select(&p.selector_table_line).next().unwrap();
+        let title_element = tr.select(&p.selector_article_head).next().unwrap();
+        let ret = p.parse_title(tr, title_element, "alternate");
+        assert!(ret.is_some());
+        let title = ret.unwrap();
+        assert_eq!(
+            title,
+            "Take action: June 2023 security update is now available"
+        );
+    }
+    #[test]
+    fn parse_title_when_self() {
+        let doc = Html::parse_document(HTML_OF_SELF_BOOKMARK);
+        let p = Parser::new("", "");
+        let tr: ElementRef<'_> = doc.select(&p.selector_table_line).next().unwrap();
+        let title_element = tr.select(&p.selector_article_head).next().unwrap();
+        let ret = p.parse_title(tr, title_element, "self");
+        assert!(ret.is_some());
+        let title = ret.unwrap();
+        assert_eq!(
+            title,
+            "Reminder: Security hardening changes for Netlogon and Kerberos coming in June and July 2023"
+        );
+    }
+    #[test]
+    fn parse() {
+        let p = Parser::new("", "");
+        let ret = p.parse(HTML_OF_EXTERNAL);
+        assert_eq!(ret.len(), 1);
+        let article = &ret[0];
+        assert_eq!(article.id, "3110");
+        assert_eq!(article.url, "https://support.microsoft.com/help/5027231");
+        assert_eq!(article.rel, "alternate");
+        assert_eq!(
+            article.title,
+            "Take action: June 2023 security update is now available"
+        );
+        assert_eq!(article.date, "2022-05-24T21:00:00Z");
+        let count_div = Regex::new(r"</?div>").unwrap();
+        assert_eq!(count_div.find_iter(&article.body).count(), 10);
+        assert!(article.body.contains(
+            r"The June 2023 security update release is now available
+    for Windows 11 and all supported versions of Windows
+    10. We recommend that you install these updates
+    promptly. For more information about the contents of
+    this update, see the release notes, which are easily
+    accessible from the"
+        ));
+        assert!(article.body.contains(
+            r"article. To learn more about the different
+    types of monthly quality updates, see"
+        ));
+        assert!(article.body.contains(
+            r"To be informed about the latest updates and
+    releases, follow us on Twitter"
+        ));
+        assert!(article
+            .body
+            .contains("Highlights for the Windows 11 update"));
+        assert!(article.body.contains("Short on time? Watch our short"));
+        assert!(article
+            .body
+            .contains("release notes video for this month's tips."));
     }
 }
